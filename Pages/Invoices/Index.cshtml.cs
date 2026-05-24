ed89b04fdbfa7545324a29ab12734cca7de4d7ba
fatmasumeyye <fatmasumeyyekoyuncu@gmail.com>
2026-05-24 23:55:37 +0300

    Update invoice index page

--- Pages/Invoices/Index.cshtml.cs
+++ Pages/Invoices/Index.cshtml.cs
@@ -1,29 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using InvoiceApp.Models;
 using InvoiceApp.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace InvoiceApp.Pages_Invoices
+namespace InvoiceApp.Pages.Invoices
 {
     public class IndexModel : PageModel
     {
-        private readonly InvoiceApp.Services.ApplicationDbContext _context;
+        private readonly ApplicationDbContext context;
 
-        public IndexModel(InvoiceApp.Services.ApplicationDbContext context)
+        public List<Invoice> invoiceList { get; set; } = new();
+
+        public IndexModel(ApplicationDbContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
-        public IList<Invoice> Invoice { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public void OnGet()
         {
-            Invoice = await _context.Invoices.ToListAsync();
+            invoiceList = context.Invoices
+                .OrderByDescending(i => i.IssueDate)
+                .ToList();
         }
     }
 }
