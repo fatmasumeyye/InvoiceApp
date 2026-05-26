ef620fee41d89e3ac557f303b3df18a27d0c6878
fatmasumeyye <fatmasumeyyekoyuncu@gmail.com>
2026-05-26 21:32:30 +0300

    Add invoice creation form validation

--- Models/InvoiceDto.cs
+++ Models/InvoiceDto.cs
@@ -1,24 +1,39 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceApp.Models
 {
     public class InvoiceDto
     {
+        [Required]
         public string? Number { get; set; } = "";
+
+        [Required]
         public string? Status { get; set; } = "";
+
         public DateOnly? IssueDate { get; set; }
+
         public DateOnly? DueDate { get; set; }
 
+        // Servis ayrıntıları
+        [Required]
         public string? Service { get; set; } = "";
 
-        [Precision(16, 2)]
+        [Range(1, 999999, ErrorMessage = "Unit price is not valid")]
         public decimal UnitPrice { get; set; }
 
+        [Range(1, 99, ErrorMessage = "Quantity is not valid")]
         public int Quantity { get; set; }
 
+        // İstemci ayrıntıları
+        [Required(ErrorMessage = "Client name is required")]
         public string? ClientName { get; set; } = "";
+
+        [Required, EmailAddress]
         public string? Email { get; set; } = "";
+
+        [Phone]
         public string Phone { get; set; } = "";
+
         public string? Address { get; set; } = "";
     }
 }
