c0588cf717396be6c20ac2983d978c0772705e95
fatmasumeyye <fatmasumeyyekoyuncu@gmail.com>
2026-05-26 22:20:49 +0300

    Add delete confirmation page

--- Pages/Invoices/Delete.cshtml.cs
+++ Pages/Invoices/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using InvoiceApp.Models;
 using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,14 +7,30 @@ namespace InvoiceApp.Pages.Invoices
 {
     public class DeleteModel : PageModel
     {
-        public ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public DeleteModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        public Invoice Invoice { get; set; } = new();
+
         public IActionResult OnGet(int id)
+        {
+            var invoice = _context.Invoices.Find(id);
+
+            if (invoice == null)
+            {
+                return RedirectToPage("/Invoices/Index");
+            }
+
+            Invoice = invoice;
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int id)
         {
             var invoice = _context.Invoices.Find(id);
 
