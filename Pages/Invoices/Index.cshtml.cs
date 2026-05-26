549fbe991ee7d93219b68165f73bfd76a92f7ea3
fatmasumeyye <fatmasumeyyekoyuncu@gmail.com>
2026-05-26 21:52:40 +0300

    Add pagination to invoice index page

--- Pages/Invoices/Index.cshtml.cs
+++ Pages/Invoices/Index.cshtml.cs
@@ -6,19 +6,57 @@ namespace InvoiceApp.Pages.Invoices
 {
     public class IndexModel : PageModel
     {
-        public List<Invoice> invoiceList = new();
-
         private readonly ApplicationDbContext context;
+
+        public List<Invoice> invoiceList { get; set; } = new();
+
+        public int PageIndex { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 10;
+
+        public List<int> PageSizeOptions { get; set; } = new() { 5, 10, 20, 50 };
 
         public IndexModel(ApplicationDbContext context)
         {
             this.context = context;
         }
 
-        public void OnGet()
+        public void OnGet(int pageIndex = 1, int pageSize = 10)
         {
-            invoiceList = context.Invoices
-                .OrderByDescending(i => i.Id)
+            if (!PageSizeOptions.Contains(pageSize))
+            {
+                pageSize = 10;
+            }
+
+            PageSize = pageSize;
+
+            var query = context.Invoices
+                .OrderByDescending(i => i.Id);
+
+            int totalRecords = query.Count();
+
+            TotalPages = (int)Math.Ceiling(totalRecords / (double)PageSize);
+
+            if (TotalPages == 0)
+            {
+                TotalPages = 1;
+            }
+
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            if (pageIndex > TotalPages)
+            {
+                pageIndex = TotalPages;
+            }
+
+            PageIndex = pageIndex;
+
+            invoiceList = query
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize)
                 .ToList();
         }
     }
